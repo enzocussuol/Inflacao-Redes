1b020dc0a2a19dbec76b2c4807bda85ba8f33aa5
Alex Oliveira <alexskntks@outlook.com>
2021-04-12 08:57:06 -0300

    Create rtt.h

---
+++ rtt.h
@@ -0,0 +1,16 @@
+#ifndef _RTT_
+#define _RTT_
+
+struct rtt{
+	int servidor;
+	int cliente;
+	double* rtt_estrela;//todos valores passando por monitores
+	double rtt_normal;
+	double inflacao;
+};
+
+
+1 4 6.5555
+0 1 9,333
+
+#endif
